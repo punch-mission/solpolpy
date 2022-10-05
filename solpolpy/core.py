454f30d17820724cf7c6ed0d333318aac28fd8de
jmbhughes <hughes.jmb@gmail.com>
2022-10-05 08:20:21 -0600

    corrected formatting

--- solpolpy/core.py
+++ solpolpy/core.py
@@ -14,75 +14,75 @@ def resolve(input_data, out_polarize_state, separation=None, alpha=None, Error=F
     input_data : Dict[str, np.ndarray]
         Dictionary formatted as follows:
 
-        - Stokes dictionary
-        "I":np.array - Should be included as a triplet of I,Q,U, and optionally V
-        "Q":np.array - Should be included as a triplet of I,Q,U, and optionally V
-        "U":np.array - Should be included as a triplet of I,Q,U, and optionally V
-        "V":np.array - Should be included as a triplet of I,Q,U, and optionally V
+            - Stokes dictionary
+                "I":np.array - Should be included as a triplet of I,Q,U, and optionally V
+                "Q":np.array - Should be included as a triplet of I,Q,U, and optionally V
+                "U":np.array - Should be included as a triplet of I,Q,U, and optionally V
+                "V":np.array - Should be included as a triplet of I,Q,U, and optionally V
 
-        - Brightness & Polarized brightness dictionary
-        "B":np.array - Should be included as a double of B, pB
-        "pB":np.array - Should be included as a doublet of B, pB
+            - Brightness & Polarized brightness dictionary
+                "B":np.array - Should be included as a double of B, pB
+                "pB":np.array - Should be included as a doublet of B, pB
 
-        - Radial & tangential brightness dictionary
-        "Br":np.array - Should be included as a doublet of Bt, Br
-        "Bt":np.array - Should be included as a doublet of Bt, Br
+            - Radial & tangential brightness dictionary
+                "Br":np.array - Should be included as a doublet of Bt, Br
+                "Bt":np.array - Should be included as a doublet of Bt, Br
 
-        - MZP triplet dictionary
-        "M":np.array - Should be included as a triplet of M,Z,P
-        "Z":np.array - Should be included as a triplet of M,Z,P
-        "P":np.array - Should be included as a triplet of M,Z,P
+            - MZP triplet dictionary
+                "M":np.array - Should be included as a triplet of M,Z,P
+                "Z":np.array - Should be included as a triplet of M,Z,P
+                "P":np.array - Should be included as a triplet of M,Z,P
 
-        - Angular dictionary [where keys are ints or floats representing
-        polarizer angle]
-        X1:np.array[xn] - Should be included as at least a triplet of angles
-        X2:np.array[xn] - Should be included as at least a triplet of angles
-        X3:np.array[xn] - Should be included as at least a triplet of angles
-        ...                               ...
-        Xn:np.array[xn] - Should be included as at least a triplet of angles
+            - Angular dictionary [where keys are ints or floats representing
+            polarizer angle]
+                X1:np.array[xn] - Should be included as at least a triplet of angles
+                X2:np.array[xn] - Should be included as at least a triplet of angles
+                X3:np.array[xn] - Should be included as at least a triplet of angles
+                ...                               ...
+                Xn:np.array[xn] - Should be included as at least a triplet of angles
 
     out_polarize_state : string
       This is the polarization state you want to convert your input
       dataframes to.  These include:
 
-    - Stokes: convert the input dataframes to output Stokes dataframes. If
-        3 polarization angles are input, or can be derived, the I, Q, and U
-        Stokes parameters are output. If 4 or more polarization angles are
-        provided, and the angles are conducsive, the full I,Q,U, and V
-        Stokes parameters are provided.
+        - Stokes: convert the input dataframes to output Stokes dataframes. If
+            3 polarization angles are input, or can be derived, the I, Q, and U
+            Stokes parameters are output. If 4 or more polarization angles are
+            provided, and the angles are conducsive, the full I,Q,U, and V
+            Stokes parameters are provided.
 
-    - B: converts input dataframes into B ("unpolarized
-        brightness") parameters.
+        - B: converts input dataframes into B ("unpolarized
+            brightness") parameters.
 
-    - pB: converts input dataframes into pB ("polarized brightness")
-        parameters.
+        - pB: converts input dataframes into pB ("polarized brightness")
+            parameters.
 
-    - Bt: Produces "Bt", the radiance observed through a linear polarizer
-        oriented tangentially to a solar-concentric circle passing through
-        the image point of interest.
+        - Bt: Produces "Bt", the radiance observed through a linear polarizer
+            oriented tangentially to a solar-concentric circle passing through
+            the image point of interest.
 
-    - Br: Produces "Br" the radiance observed through a linear polarizer
-        oriented radially to the centre Sun through the same point.
+        - Br: Produces "Br" the radiance observed through a linear polarizer
+            oriented radially to the centre Sun through the same point.
 
-    - BrBt: Produces both the  radiance observed through a linear polarizer
-        oriented tangentially to a solar-concentric circle passing through
-        the image point of interest "Bt", and the radiance observed through
-        a linear polarizer oriented radially to the centre Sun through the
-        same point "Br".
+        - BrBt: Produces both the  radiance observed through a linear polarizer
+            oriented tangentially to a solar-concentric circle passing through
+            the image point of interest "Bt", and the radiance observed through
+            a linear polarizer oriented radially to the centre Sun through the
+            same point "Br".
 
-    - 3pol or MZP: converts input dataframes into a system of virtual
-        polarizer triplets each separated by 60 degrees (Minus [-60],
-        Zero [0], Plus [60]).
+        - 3pol or MZP: converts input dataframes into a system of virtual
+            polarizer triplets each separated by 60 degrees (Minus [-60],
+            Zero [0], Plus [60]).
 
-    - 4pol: converts input dataframes into a system of virtual polarizer
-        triplets each separated by 45 degrees at -45, 0, 45, 90.
+        - 4pol: converts input dataframes into a system of virtual polarizer
+            triplets each separated by 45 degrees at -45, 0, 45, 90.
 
-    - Xpol: converts input dataframes into a system of virtual polarizer
-        triplets each separated by X degrees, specified by the optional
-        input separation, which should be input in degrees.
+        - Xpol: converts input dataframes into a system of virtual polarizer
+            triplets each separated by X degrees, specified by the optional
+            input separation, which should be input in degrees.
 
-    - BpB: converts input dataframes into two dataframes, B ("unpolarized
-        brightness") parameter and its counterpart pB ("polarized brightness")
+        - BpB: converts input dataframes into two dataframes, B ("unpolarized
+            brightness") parameter and its counterpart pB ("polarized brightness")
 
     Raises
     ------
--- tests/test_core.py
+++ tests/test_core.py
@@ -6,5 +6,6 @@ pytest test suite for the polarizers module of solpolpy
 import numpy as np
 import pytest
 
+
 def test_true():
     assert True

aa0d31352aad7e8e4e548d0eca77f01f40efd834
s0larish <risht42@gmail.com>
2023-06-08 16:37:42 -0600

    Updated alpha.py

--- solpolpy/alpha.py
+++ solpolpy/alpha.py
@@ -17,6 +17,17 @@ def radial90(shape):
     return np.fliplr(np.rot90(np.arctan2(yy, xx), k=1))*u.radian
 
 
+def radial_west(shape):
+    '''
+    assumes solar north is up
+    creates radial polarization map
+    '''
+    x_size, y_size = shape
+    x = np.arange(-x_size // 2, x_size // 2)
+    y = np.arange(-y_size // 2, y_size // 2)
+    xx, yy = np.meshgrid(x, y)
+    return np.flipud(np.fliplr(np.arctan2(yy, xx) + np.pi))*u.radian
+
 def radial_north(shape):
     '''
     assumes solar north is up
@@ -27,20 +38,7 @@ def radial_north(shape):
     y = np.arange(-y_size // 2, y_size // 2)
     xx, yy = np.meshgrid(x, y)
     # return np.fliplr(np.arctan2(yy, xx))*u.radian
-    return np.rot90(np.fliplr(np.arctan2(yy, xx) + np.pi), k=1) * u.radian
-
-
-def radial_west(shape):
-    '''
-    assumes solar north is up
-    creates radial polarization map
-    '''
-    x_size, y_size = shape
-    x = np.arange(-x_size // 2, x_size // 2)
-    y = np.arange(-y_size // 2, y_size // 2)
-    xx, yy = np.meshgrid(x, y)
-    return np.fliplr(np.arctan2(yy, xx) + np.pi)*u.radian
-    # return np.rot90(np.fliplr(np.arctan2(yy, xx) + np.pi), k=1) * u.radian
+    return np.flipud(np.rot90(np.fliplr(np.arctan2(yy, xx) + np.pi), k=1))* u.radian
 #
 
 def zeros(shape):

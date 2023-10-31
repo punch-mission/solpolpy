352ccb28bcb56e31e6e28bc840965afb71541090
s0larish <risht42@gmail.com>
2023-10-30 20:57:36 -0600

    updated functions and readme

--- solpolpy/constants.py
+++ solpolpy/constants.py
@@ -5,4 +5,5 @@ VALID_KINDS = {"MZP": [["Bm", "Bz", "Bp", "alpha"], ["Bm", "Bz", "Bp"]],
                "Stokes": [["Bi", "Bq", "Bu"]],
                "Bp3": [["B", "pB", "pBp", "alpha"], ["B", "pB", "pBp"]],
                "Bthp": [["B", "theta", "p"]],
-               "npol": [["angle_1", "angle_2", "angle_3"], ["angle_1", "angle_2", "angle_3", "angle_4"]]}
+               "npol": [["angle_1", "angle_2", "angle_3"]],
+               "fourpol": [["angle_1", "angle_2", "angle_3", "angle_4"]]}
